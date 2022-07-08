9d0978bd4f4df511ba3fa5d13a1d93cfc0d7cbd5
Nguyen Thanh Bien <bien@bienlab.com>
2022-07-08 13:53:51 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 var port = process.env.PORT || 3000;
-
-//mongoose.connect('mongodb://localhost:27017/dbname');
 
 var router = express.Router();
 
